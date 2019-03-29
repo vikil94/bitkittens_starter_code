15578e8a5f5ebcce59375200d40ca1c97c72ba6a
Vikil Naik <vikilnaik37@gmail.com>
2019-03-29 15:38:32 -0400

    Finished excercise

--- bitkittens/static/js/cats.js
+++ bitkittens/static/js/cats.js
@@ -1,2 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
+
+    const summonBtn = document.querySelector('.summon-cats');
+    const catBoxes = document.querySelectorAll('.cat-box');
+
+
+    summonBtn.addEventListener('click', () => {
+        console.log('Summon Kittens button was clicked')
+        axios.get('http://bitkittens.herokuapp.com/cats.json')
+            .then((response) => {
+
+                catBoxes.forEach((box, i) => {
+                    box.innerHTML = '';
+                    const cat = document.createElement('img');
+                    cat.src = response.data.cats[i].photo;
+                    cat.alt = `Photo of ${ response.data.cats[i].name }`;
+                    box.appendChild(cat);
+
+                });
+            });
+
+    });
+
 });
